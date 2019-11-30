cbb1a554f113526356ff67734ab96169d053110e
Himesh(Git) <adithra_git@outlook.com>
2019-11-30 15:12:02 +0530

    Add project files.

---
+++ PrimeFactor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeFactor_TDD
+{
+    public static class PrimeFactor
+    {
+        public static List<int> Genarate(int number)
+        {
+            var numbers = new List<int>();
+            var divider = 2;
+            while (number > 1)
+            {
+                //start:
+                //if (number % divider != 0)
+                //{
+                //    goto start;
+                //}
+
+
+                while (number % divider == 0)
+                {
+                    numbers.Add(divider);
+                    number = number / divider;
+                    //goto start;
+
+                }
+                divider++;
+
+            }
+            if (number > 1)
+            {
+                numbers.Add(number);
+            }
+
+            return numbers;
+        }
+    }
+}
---
+++ PrimeFactorTest.cs
@@ -0,0 +1,94 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeFactor_TDD
+{
+    [TestFixture]
+    public class PrimeFactorTest
+    {
+        private List<int> List(params int[] numbers)
+        {
+            var newNumbers = new List<int>();
+            foreach (var n in numbers)
+            {
+                newNumbers.Add(n);
+            }
+            return newNumbers;
+        }
+
+
+        [Test]
+
+        public void TestOne()
+        {
+            Assert.That(PrimeFactor.Genarate(1),Is.EqualTo(new List<int>()));
+        }
+
+        [Test]
+        public void TestTwo()
+        {
+            Assert.That(PrimeFactor.Genarate(2), Is.EqualTo(List( 2)));
+
+        }
+
+        [Test]
+        public void TesThree()
+        {
+            Assert.That(PrimeFactor.Genarate(3), Is.EqualTo(List( 3 )));
+
+        }
+
+        [Test]
+        public void TestFour()
+        {
+            Assert.That(PrimeFactor.Genarate(4), Is.EqualTo(List(2,2 )));
+
+        }
+
+        [Test]
+        public void TestFive()
+        {
+            Assert.That(PrimeFactor.Genarate(5), Is.EqualTo(List(5 )));
+
+        }
+
+        [Test]
+        public void TestSix()
+        {
+            Assert.That(PrimeFactor.Genarate(6), Is.EqualTo(List(2,3 )));
+
+        }
+
+        [Test]
+        public void TestSeven()
+        {
+            Assert.That(PrimeFactor.Genarate(7), Is.EqualTo(List(7 )));
+
+        }
+
+        [Test]
+        public void TestEight()
+        {
+            Assert.That(PrimeFactor.Genarate(8), Is.EqualTo(List(2, 2,2 )));
+
+        }
+
+        [Test]
+        public void TestNine()
+        {
+            Assert.That(PrimeFactor.Genarate(9), Is.EqualTo(List(3,3)));
+
+        }
+
+        [Test]
+        public void TestMain()
+        {
+            Assert.That(PrimeFactor.Genarate(84), Is.EqualTo(List(2, 2, 3, 7)));
+
+        }
+    }
+}
